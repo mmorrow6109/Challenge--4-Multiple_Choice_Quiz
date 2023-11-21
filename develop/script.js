287d87c9e71c01da56dd02c881c58ca50213d964
mmorrow6109 <msmorrow6109@gmail.com>
2023-11-20 18:30:40 -0800

    consolidated back to single HTML

--- develop/script.js
+++ develop/script.js
@@ -1,10 +1,10 @@
-let startButton = document.getElementById("start-btn");
-let nextButton = document.getElementById("next-btn");
-let questionElement = document.querySelector("#question");
-let choices = Array.from(document.querySelectorAll("choice-buttons"));
-let timer = document.querySelector("#timer");
-let scoreText = document.querySelector("#score");
-let finalScore = document.querySelector("#finalScore");
+const startButton = document.getElementById("start-btn");
+const nextButton = document.getElementById("next-btn");
+const questionElement = document.querySelector("#question");
+const choices = Array.from(document.querySelectorAll("choice-buttons"));
+const timer = document.querySelector("#timer");
+const scoreText = document.querySelector("#score");
+const finalScore = document.querySelector("#finalScore");
 
 let currentQuestion = {};
 let acceptingAnswers = true;
@@ -12,26 +12,26 @@ let score = 0;
 let questionCounter = 0;   
 let availableQuestions = [];
 
-let questions = [
+const questions = [
     {
         question: "What is an array?",
         choice1: "A pack of ravenous moose",
         choice2: "An unordered list of random items",
         choice3: "A collection of data",
-        choice4: "Is mayonnaise an instrument?",
+        choice4: "idk lol",
         answer: 3,
     },
     {
         question: "What is JavaScript?",
         choice1: "A series of unfortunate events",
         choice2: "A coffee stained film script",
-        choice3: "is mayonnaise an instrument?",
+        choice3: "idk lol",
         choice4: "A language used to program web pages",
         answer: 4,
     },
     {
         question: "What term does NOT belong?",
-        choice1: "Is mayonnaise an instrument?",
+        choice1: "idk lol",
         choice2: ".CSS",
         choice3: ".JS",
         choice4: ".HTML",
@@ -42,7 +42,7 @@ let questions = [
         choice1: "It causes the sun to rise and set",
         choice2: "It is used to program web pages",
         choice3: "It's not. Computers are lame",
-        choice4: "Is mayonnaise an instrument?",
+        choice4: "idk lol",
         answer: 2,
     },
     {
@@ -50,7 +50,7 @@ let questions = [
         choice1: "function = myFunction()",
         choice2: "function:myFunction()",
         choice3: "function myFunction()",
-        choice4: "Is mayonnaise an instrument?",
+        choice4: "idk lol",
         answer: 1,
     },
 ]
@@ -63,19 +63,97 @@ function startGame() {
     questionCounter = 0;
     score = 0;
     shuffledQuestions = questions.sort(() => Math.random() - .5);
-    getNewQuestion();   
+    getNewQuestion();
+    startTimer();
 }
 
 function getNewQuestion() {
+    // resetState();
     showQuestion(shuffledQuestions[questionCounter]);
 }
 
+// function resetState() {
+//     clearStatusClass(document.body);
+//     // nextButton.classList.add("hidden");
+//     while (answerButtonsElement.firstChild) {
+//         answerButtonsElement.removeChild
+//         (answerButtonsElement.firstChild);
+//     }
+// }
+
 function showQuestion(questions) {
+    const button = document.querySelectorAll("choice");
     questionElement.innerText = questions.question;
-    choice1.innerText = questions.choice1;
-    choice2.innerText = questions.choice2;
-    choice3.innerText = questions.choice3;
-    choice4.innerText = questions.choice4;
+    question.choices.forEach(choice => {
+        button.innerText = choice.text;
+        button.classList.add("btn-grid");
+        if (answer.correct) {
+            button.dataset.answer = answer.correct;
+        }
+        button.addEventListener("click", selectAnswer);
+        choices.appendChild(button);
+    })
 }
 
+
+// function selectAnswer(e) {
+//     const selectedButton = e.target;
+//     const correct = selectedButton.dataset.correct;
+//     setStatusClass(document.body, correct);
+//     Array.from(choices.children).forEach(button => {
+//         setStatusClass(button, button.dataset.correct);
+//     })
+//     if (shuffledQuestions.length > questionCounter + 1) {
+//         nextButton.classList.remove("hidden");
+//     } else {
+//         startButton.innerText = "Restart";
+//         startButton.classList.remove("hidden");
+//     }
+
+//     return window.location.assign("end.html");
+// }
+
+// function setStatusClass(element, correct) {
+//     clearStatusClass(element);
+//     if (correct) {
+//         element.classList.add("correct");
+//     } else {
+//         element.classList.add("wrong");
+//     }
+// }
+
+// function clearStatusClass(element) {
+//     element.classList.remove("correct");
+//     element.classList.remove("wrong");
+// }
+
+function startTimer() {
+    let timeLeft = 90;
+    let timeInterval = setInterval(function() {
+        if (timeLeft > 1) {
+            timer.textContent = timeLeft + " seconds remaining";
+            timeLeft--;
+        } else if (timeLeft === 1) {
+            timer.textContent = timeLeft + " second remaining";
+            timeLeft--;
+        } else {
+            timer.textContent = "";
+            clearInterval(timeInterval);
+            // displayMessage();
+        }
+    }, 1000);
+}
+
+// function displayMessage() {
+//     timer.textContent = "Time's up!";
+//     setTimeout(function() {
+//         timer.textContent = "";
+//     }, 1000);
+//     return window.location.assign("end.html");
+// }
+
 startButton.addEventListener("click", startGame);
+// nextButton.addEventListener("click", () => {
+//     questionCounter++;
+//     getNewQuestion();
+// })
